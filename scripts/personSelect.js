9a063f21d82784882bc1778ac91cbbb91e518f2e
tkoch96 <tomkoch96@yahoo.com>
2015-02-14 23:56:12 -0500

    Fixed button not bringing to form issue

--- scripts/personSelect.js
+++ scripts/personSelect.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	
 	$('.people').addClass('ui basic blue button');
-	$('.people a').click(function(event) {
+	$('.people').click(function(event) {
+		event.preventDefault();
+		location.hash ='#form';
+		
 		var $name = $(this).text();
 		$('#profname').attr('value',$name);
 		$('#title-blurb').text('Tell us what you think about ' + $name);
